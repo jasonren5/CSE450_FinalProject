4787ffa8554028ccba13807fb131140d95f47417
extondev <31326470+extondev@users.noreply.github.com>
2020-02-16 20:25:52 -0600

    added rigidbody to player object, changed movement
    movement should hopefully be framerate independent now, and uses acceleration based movement rather than fixed velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,34 +6,49 @@ public class PlayerController : MonoBehaviour
 {
     //speed of the player character
     public float speed;
+    public float maxSpeed;
+    float sqrMaxSpeed;
+
+    
+    Rigidbody2D rb;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get rigidbody component at start to avoid excess calls to getComponent
+        rb = GetComponent<Rigidbody2D>();
+        //accessing sqrMagnitude is quicker than magnitude, so use that when checking speed and then square maxSpeed
+        sqrMaxSpeed = maxSpeed * maxSpeed;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, speed, 0);
+            rb.velocity += speed * Vector2.up * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-speed, 0, 0);
+            rb.velocity += speed * Vector2.left * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, -speed, 0);
+            rb.velocity += speed * Vector2.down * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(speed, 0, 0);
+            rb.velocity += speed * Vector2.right * Time.deltaTime;
+        }
+
+        
+        if (rb.velocity.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
         }
     }
 }
