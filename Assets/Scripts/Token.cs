3e8725949ed4db9275c96ea1de8efd0f0cb6dbed
DESKTOP-M8L5RJS\timst <cpstevens@wustl.edu>
2020-02-19 14:18:15 -0600

    Changed Pokemon enemy Sprite to Gastly and added tokens

---
+++ Assets/Scripts/Token.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Token : MonoBehaviour
+{
+    //Set up collision event with the player to destroy the token
+    void OnCollisionEnter2D(Collision2D other) {
+        //Reload scene only when colliding with player
+        if(other.gameObject.GetComponent<PlayerController>()) {
+            Destroy(gameObject);
+        }
+    }
+}
