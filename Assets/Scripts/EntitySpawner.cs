cf27689d834898ab9c09d9a627bea4bc05321428
extondev <31326470+extondev@users.noreply.github.com>
2020-04-01 20:04:33 -0400

    added to DonkeyKong scene, created EntitySpawner.cs

---
+++ Assets/Scripts/EntitySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySpawner : MonoBehaviour
+{
+    public GameObject entityToSpawn;
+    public float spawnDelay;
+    float lastSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastSpawn = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > lastSpawn + spawnDelay)
+        {
+            Instantiate(entityToSpawn);
+            lastSpawn = Time.time;
+        }
+    }
+}
