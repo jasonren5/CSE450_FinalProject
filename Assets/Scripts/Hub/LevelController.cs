3beb35003a73232be26aa3f39a8f696da6f6a215
Claire Heinrich <claireheinrich@wustl.edu>
2020-04-12 09:47:31 -0500

    Layout of video game hub

---
+++ Assets/Scripts/Hub/LevelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public static LevelController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadDonkeyKong()
+    {
+        SceneManager.LoadScene("DonkeyKong");
+    }
+
+    public void LoadPacMan()
+    {
+        SceneManager.LoadScene("Pacman-2.0");
+    }
+
+    public void LoadPokemon()
+    {
+        SceneManager.LoadScene("Pokemon");
+    }
+}
