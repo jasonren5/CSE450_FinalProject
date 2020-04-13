62eedb1a9d5850bb79c2baeca75b92573c23be4e
Claire Heinrich <claireheinrich@wustl.edu>
2020-04-12 21:17:38 -0500

    added outlets to playerprefs

--- Assets/Scripts/Hub/HubLevelSelector.cs
+++ Assets/Scripts/Hub/HubLevelSelector.cs
@@ -14,6 +14,5 @@ public class HubLevelSelector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
--- Assets/Scripts/Hub/LevelController.cs
+++ Assets/Scripts/Hub/LevelController.cs
@@ -1,36 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
-    public static LevelController instance;
+    public Text pokemonBest;
+    public Text pacManBest;
+    public Text donkeyKongBest;
 
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void LoadDonkeyKong()
-    {
-        SceneManager.LoadScene("DonkeyKong");
-    }
-
-    public void LoadPacMan()
-    {
-        SceneManager.LoadScene("Pacman-2.0");
-    }
-
-    public void LoadPokemon()
-    {
-        SceneManager.LoadScene("Pokemon");
+        pokemonBest.text = PlayerPrefs.GetFloat("pokemonBestScore").ToString();
+        pacManBest.text = PlayerPrefs.GetFloat("pacManBestScore").ToString();
+        donkeyKongBest.text = PlayerPrefs.GetFloat("donkeyKongBestScore").ToString();
+        //TODO: Add conditionals to check if time or score should be shown
+        //TODO: Change "score" to be "time" and add measurements like "seconds" when switched to time
+        //pokemonBest.text = PlayerPrefs.GetFloat("pokemonBestTime").ToString();
+        //pacManBest.text = PlayerPrefs.GetFloat("pacManBestTime").ToString();
+        //donkeyKongBest.text = PlayerPrefs.GetFloat("donkeyKongBestTime").ToString();
     }
 }
