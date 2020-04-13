df9023617bddc7ef08dc46f627c7674a40a3be0f
jasonren5 <jasonren72@gmail.com>
2020-04-12 21:40:43 -0400

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public static GameController GM;
+    public static GameController instance;
 
     public int score;
     public bool[] pokemon_collectables = new bool[8];
 
     void Awake()
     {
-        if (GM != null)
-            GameObject.Destroy(GM);
-        else
-            GM = this;
-
-        DontDestroyOnLoad(this);
+        instance = this;
     }
 }
